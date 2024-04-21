c7e68c08e04b25de994cf26228b26357d6e12287
Ismoiljondev <ismoiljonhalilov5@gmail.com>
2024-04-21 22:53:27 +0500

    intalling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
-	return <Button variant={"ghost"}>Submit</Button>;
+	return (
+		<>
+			<Label htmlFor="email">Email</Label>
+			<Input id="email" type="email" placeholder="Email"/>
+			<Button variant={"ghost"}>Submit</Button>
+		</>
+	);
 }
